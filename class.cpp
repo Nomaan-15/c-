6a3408a682738fc38d1b4a4d8396a159ac134934
Nomaan00 <nomaanmohammed82@gmail.com>
2024-01-06 14:41:57 +0530

    c++new file

---
+++ class.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+class student{
+
+    int stu_id;
+    int name;
+
+    void getDetails(){
+        cout<<stu_id<<endl;
+        
+    }
+};
+
+int main(){
+
+    student s;
+    s.getDetails();
+}
